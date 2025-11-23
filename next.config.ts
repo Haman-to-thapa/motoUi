30a31406b49288439cf724af25fdc8e49c99fa16
Haman-to-thapa <hamanthapa0000@gmail.com>
2025-11-23 12:49:49 +0530

    fix: update start script to use next start

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
-  reactStrictMode: true,
-  compress: true,
-  productionBrowserSourceMaps: false,
   images: {
     unoptimized: true
   }
 }
-export default nextConfig;
+
+export default nextConfig
